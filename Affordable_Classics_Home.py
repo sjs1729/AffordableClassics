38d70bad834b88e15da82ab34723b2a320af453c
sjs1729 <87244944+sjs1729@users.noreply.github.com>
2025-01-10 12:36:38 +0530

    Add files via upload

---
+++ Affordable_Classics_Home.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime, timedelta
+from io import BytesIO
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+
+# Path or URL for your logo
+LOGO_PATH = "AC_Logo.png"  # Replace with the actual path or URL
+CSV_FILE = "SizeChart.csv"    # Replace with the actual CSV file name
+
+
+st.set_page_config(layout="wide")
+
+st.markdown(
+    """
+    <style>
+    .block-container {
+        padding-top: 2rem !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+c1,c2,c3 = st.columns((10,2,10))
+c2.image(LOGO_PATH, width=85)  # Adjust width as needed
+
+
+st.markdown('<p style="font-size:40px;font-weight: bold;text-align:center;vertical-align:middle;color:blue;margin:0px;padding:0px">Affordable Classics</p>', unsafe_allow_html=True)
+
+html_text = '<BR><div style="font-family: Arial, sans-serif; font-size: 1.1em; line-height: 1.5;color:#941751"> \
+  <strong>Experience the luxury of a custom-tailored, linen-blend shirt designed just for you—without breaking the bank.</strong><br> \
+  Our bespoke creations, complete with optional hand-embroidered initials, bring comfort, style, and personal flair right to your doorstep. \
+  <em>Place your order today and embrace the elegance of Affordable Classics.</em></div><BR><BR>'
+
+st.markdown(html_text, unsafe_allow_html=True)
+
+
+c1,buf,c2 = st.columns((26,3,12))
+c2.image("Shirt_White.png", width=200)  # Adjust width as needed
+
+html_subtext = '<BR><BR><div style="font-family: Arial, sans-serif; font-size: 1.0em; line-height: 1.5; color:#941751"> \
+  Currently we are offering the  choice of colours - <span><strong>Magnificient White</strong></span>, <span><strong>Navy Blue</strong></span>, <span><strong>Indigo</strong></span> & <span><strong>Aqua Blue</strong></span>.<br><br> \
+  The Linen blend fabric is so comfortable that you will never want to take it off. <br><br>\
+  <em>Hurry, place your order now (<span><strong>Side Menu --> Buy a Shirt</strong></span>) and be a part of <strong><span>Affordable Classics</strong></span>.</em></div><BR><BR>'
+
+c1.markdown(html_subtext, unsafe_allow_html=True)
---
+++ db_functions.py
@@ -0,0 +1,172 @@
+import pandas as pd
+import mysql.connector
+from mysql.connector import Error
+import streamlit as st
+
+
+# Database connection details
+db_config = {
+    'host': '107.180.118.206',
+    'user': 'growealth',
+    'password': 'growealth@123',
+    'database': 'growealth',
+}
+
+# Function to connect to the MySQL database
+def connect_to_database():
+    try:
+        connection = mysql.connector.connect(**db_config)
+        return connection
+    except Error as e:
+        st.error(f"Error connecting to database: {e}")
+        return None
+
+
+def insert_order(order_no, curr_submission, shirt_dims, additional_notes, curr_status):
+
+    #st.write("Checking Connected")
+    #return 0
+    # Connect to the MySQL database
+    connection = connect_to_database()
+
+    if connection.is_connected():
+        # Fetch data using Pandas
+
+        #st.write("Connected")
+
+
+        insert_query = """
+            INSERT INTO CUSTOMER_ORDERS (
+              ORDER_NUMBER,
+              CUSTOMER_NAME,
+              MOBILE_NO,
+              EMAIL,
+              DELIVERY_ADDR,
+              SHIRT_COLOUR,
+              CHEST_SIZE,
+              HOW_TALL,
+              BODY_TYPE,
+              POCKETS,
+              HEMLINE,
+              HALF_SLEEVE,
+              SHIRT_LENGTH,
+              ACROSS_SHOULDER,
+              WAIST,
+              COLLAR,
+              SLEEVE_LENGTH,
+              ADDITIONAL_NOTES,
+              ORDER_STATUS,
+              SHIRT_FIT
+            )
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+        """
+
+        if curr_submission["half_sleeve"]:
+            half_sleeve = "Y"
+        else:
+            half_sleeve = "N"
+
+        # Create a cursor from the existing connection
+        cursor = connection.cursor()
+        try:
+            # Execute the insert query with parameter binding
+            cursor.execute(
+                insert_query,
+                (
+                    order_no,
+                    curr_submission["name"],
+                    curr_submission["mobile_number"],
+                    curr_submission["email"],
+                    curr_submission["delivery_addr"],
+                    curr_submission["color_option"],
+                    curr_submission["chest_size"],
+                    curr_submission["how_tall"],
+                    curr_submission["body_type"],
+                    curr_submission["pockets"],
+                    curr_submission["hemline"],
+                    half_sleeve,
+                    shirt_dims["ShirtLength"],
+                    shirt_dims["AcrossShoulder"],
+                    shirt_dims["Waist"],
+                    shirt_dims["Collar"],
+                    shirt_dims["SleeveLength"],
+                    additional_notes,
+                    curr_status,
+                    curr_submission["shirt_fit"]
+                )
+            )
+            # Commit the transaction
+            connection.commit()
+            st.success("Customer Order inserted successfully!")
+        except Exception as e:
+            connection.rollback()
+            st.error(f"Error inserting order: {e}")
+            return -1
+
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+                return 0
+    else:
+
+        return -1
+
+
+@st.cache_data()
+def fetch_all_orders():
+    """
+    Fetches a dataset from a remote MySQL database based on a query.
+
+    Args:
+        query (str): The SQL query to execute.
+
+    Returns:
+        pd.DataFrame: The resulting dataset as a Pandas DataFrame.
+    """
+    try:
+        #st.write("function called")
+        # Connect to the MySQL database
+        connection = connect_to_database()
+
+        if connection.is_connected():
+            # Fetch data using Pandas
+            df = pd.read_sql("SELECT * FROM CUSTOMER_ORDERS", connection)
+            return df
+
+    except Error as e:
+        print(f"Error: {e}")
+        return None
+
+    finally:
+        if connection.is_connected():
+            connection.close()
+
+
+def fetch_past_orders(mobile_no):
+    """
+    Fetches a dataset from a remote MySQL database based on a query.
+
+    Args:
+        query (str): The SQL query to execute.
+
+    Returns:
+        pd.DataFrame: The resulting dataset as a Pandas DataFrame.
+    """
+    try:
+        #st.write("function called")
+        # Connect to the MySQL database
+        connection = connect_to_database()
+
+        if connection.is_connected():
+            # Fetch data using Pandas
+            df = pd.read_sql(f"SELECT * FROM CUSTOMER_ORDERS WHERE MOBILE_NO = '{mobile_no}'", connection)
+            return df
+
+    except Error as e:
+        print(f"Error: {e}")
+        return None
+
+    finally:
+        if connection.is_connected():
+            connection.close()
