f1133fb962096ccee4a25aa2f6f455028ee133b1
sjs1729 <87244944+sjs1729@users.noreply.github.com>
2025-09-28 17:42:46 +0530

    Add files via upload

--- Affordable_Classics_Home.py
+++ Affordable_Classics_Home.py
@@ -28,14 +28,6 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Hide Streamlit menu and footer
-hide_streamlit_style = """
-        <style>
-        .stToolbarActions {display: none !important;}
-        </style>
-        """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
 
 if "authenticated" not in st.session_state:
     st.session_state.authenticated = False
@@ -51,12 +43,16 @@ c2.markdown('<BR>',unsafe_allow_html=True)
 c2.image(LOGO_PATH, width=85)  # Adjust width as needed
 
 
-st.markdown('<p style="font-size:40px;font-weight: bold;text-align:center;vertical-align:middle;color:blue;margin:0px;padding:0px">Affordable Classics</p>', unsafe_allow_html=True)
+st.markdown('<p style="font-size:40px;font-weight: bold;text-align:center;vertical-align:middle;color:#1877F2;margin:0px;padding:0px">Affordable Classics</p>', unsafe_allow_html=True)
 
-html_text = '<BR><div style="font-family: Arial, sans-serif; font-size: 1.1em; line-height: 1.5;color:#941751"> \
-  <strong>Experience the luxury of a custom-tailored, linen-blend shirt designed just for you—without breaking the bank.</strong><br> \
-  Our bespoke creations, bring comfort, style and personal flair right to your doorstep. \
-  <em>Place your order today and embrace the elegance of Affordable Classics.</em></div><BR><BR>'
+html_text = '<BR><div style="font-family: Arial, sans-serif; font-size: 1.1em; line-height: 1.5;color:#606770"> \
+  <strong>Experience the luxury of a custom-tailored at an affordable price.</strong><br><br> \
+  At <strong>Affordable Classics</strong>, we believe your style should fit you, not the other way around. \
+  That’s why we offer custom-fit shirts, crafted precisely to your measurements. \
+  Our bespoke creations, bring comfort, style and personal flair right to your doorstep.<BR><BR> \
+  <em>Place your order today and embrace the elegance of Affordable Classics. \
+  The collection of Linen blend & Cotton fabrics are so comfortable and trendy that you will never want to take it off.</em></div><BR><BR>'
+
 
 st.markdown(html_text, unsafe_allow_html=True)
 
@@ -64,14 +60,12 @@ st.markdown(html_text, unsafe_allow_html=True)
 c1,buf,c2 = st.columns((26,3,12))
 c2.image("Shirt_White.png", width=180)  # Adjust width as needed
 
-html_subtext = '<BR><BR><div style="font-family: Arial, sans-serif; font-size: 1.0em; line-height: 1.5; color:#941751"> \
-  Currently we are offering the  choice of colours - <span><strong>Magnificient White</strong></span>, <span><strong>Navy Blue</strong></span>, <span><strong>Indigo</strong></span> & <span><strong>Aqua Blue</strong></span>.<br><br> \
-  The Linen blend fabric is so comfortable that you will never want to take it off. <br><br>\
-  <em>Hurry, place your order now (<span><strong>Side Menu --> Buy a Shirt</strong></span>) and be a part of <strong><span>Affordable Classics</strong></span>.</em></div><BR><BR>'
+html_subtext = '<BR><BR><div style="font-family: Arial, sans-serif; font-size: 1.0em; line-height: 1.5; color:#606770"> \
+  <em>Hurry, place your order now (<span><strong>Side Menu --> Buy a Shirt</strong></span>) and be a part of <strong><span>Affordable Classics</strong></span>.</em></div>'
 
 c1.markdown(html_subtext, unsafe_allow_html=True)
 
 
-st.markdown('<BR><p style="font-size:18px;font-weight: bold;text-align:left;vertical-align:middle;color:blue;margin:0px;padding:0px">Contact:</p>', unsafe_allow_html=True)
-st.markdown(":phone: +91 9836064237")
-st.markdown(":email: [helpdesk@gro-wealth.in](emailto:saha.sanjoy@gmail.in)")
+c1.markdown('<BR><p style="font-size:18px;font-weight: bold;text-align:left;vertical-align:middle;color:#1877F2;margin:0px;padding:0px">Contact:</p>', unsafe_allow_html=True)
+c1.markdown(":phone: +91 9836064237")
+c1.markdown(":email: [saha.sanjoy@gmail.com](emailto:saha.sanjoy@gmail.com)")
